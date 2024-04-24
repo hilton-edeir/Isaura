93e63b02dbbd66a18f579150297913f0e92a2439
Hilton Tavares <hilton.edeir@gmail.com>
2024-04-24 06:05:46 +0100

    update

--- app/src/main/java/com/isaura/SignIn.java
+++ app/src/main/java/com/isaura/SignIn.java
@@ -2,24 +2,94 @@ package com.isaura;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class SignIn extends AppCompatActivity {
 
-
+    TextInputLayout lyt_email, lyt_code;
+    TextInputEditText fld_email, fld_code;
+    Button btn_sign_in;
+    ProgressBar progressBar;
+    public FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_sign_in);
         getSupportActionBar().hide();
-        Button btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(v -> {
-            startActivity(new Intent(SignIn.this, Home.class));
+
+        mAuth = FirebaseAuth.getInstance();
+
+        lyt_email = findViewById(R.id.lyt_email);
+        lyt_code = findViewById(R.id.lyt_code);
+        fld_email = findViewById(R.id.fld_email);
+        fld_code = findViewById(R.id.fld_code);
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+        progressBar = findViewById(R.id.progress_bar);
+
+        btn_sign_in.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(fld_email.getText()).toString();
+            String code = Objects.requireNonNull(fld_code.getText()).toString();
+
+            if (email.isEmpty() && code.isEmpty()) {
+                lyt_email.setHelperText("Insira o email");
+                lyt_code.setHelperText("Insira o código");
+            }
+            if (email.isEmpty()) {
+                lyt_email.setHelperText("Insira o email");
+                lyt_code.setHelperText(null);
+            }
+            else {
+                if(code.isEmpty()){
+                    lyt_email.setHelperText(null);
+                    lyt_code.setHelperText("Insira o código");
+                }
+                else {
+                    lyt_email.setHelperText(null);
+                    lyt_code.setHelperText(null);
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(email, code)
+                            .addOnCompleteListener(this, task -> {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    mAuth.getCurrentUser();
+                                    startActivity(new Intent(SignIn.this, Home.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignIn.this, "Autenticação Falhada",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
+
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
 
     public void sign_up(View view) {
--- app/src/main/java/com/isaura/SignUp.java
+++ app/src/main/java/com/isaura/SignUp.java
@@ -3,6 +3,9 @@ package com.isaura;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class SignUp extends AppCompatActivity {
+
+    TextInputLayout lyt_email, lyt_code;
+    TextInputEditText fld_email, fld_code;
+    ProgressBar progressBar;
+    Button btn_sign_up;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,53 @@ public class SignUp extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.act_sign_up);
         getSupportActionBar().hide();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        lyt_email = findViewById(R.id.lyt_email);
+        lyt_code = findViewById(R.id.lyt_code);
+        fld_email = findViewById(R.id.fld_email);
+        fld_code = findViewById(R.id.fld_code);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+        progressBar = findViewById(R.id.progress_bar);
+
+        btn_sign_up.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(fld_email.getText()).toString();
+            String code = Objects.requireNonNull(fld_code.getText()).toString();
+
+            if (email.isEmpty() && code.isEmpty()) {
+                lyt_email.setHelperText("Insira o email");
+                lyt_code.setHelperText("Insira o código");
+            }
+            if (email.isEmpty()) {
+                lyt_email.setHelperText("Insira o email");
+                lyt_code.setHelperText(null);
+            }
+            else {
+                if(code.isEmpty()){
+                    lyt_email.setHelperText(null);
+                    lyt_code.setHelperText("Insira o código");
+                }
+                else {
+                    lyt_email.setHelperText(null);
+                    lyt_code.setHelperText(null);
+                    progressBar.setVisibility(View.VISIBLE);
+                    mAuth.createUserWithEmailAndPassword(email, code)
+                            .addOnCompleteListener(this, task -> {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUp.this, "Conta criada com sucesso", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(SignUp.this, SignIn.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(SignUp.this, "Falha ao criar a conta", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+            }
+
+        });
 
     }
 
